0974d62d0a4e69623fc5fa24d67f7eadd603f18e
Glowstone91 <pedrolucasms2023@gmail.com>
2026-06-28 15:40:23 -0300

    Refactor color input handling and synchronization
    Refactor color synchronization and rendering logic. Improve handling of color input and background synchronization.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function adicionarCor() {
   box.className = "corBox";
   box.innerHTML = `
     <input type="color" value="#ffffff">
-    <input type="text" value="#ffffff" maxlength="7">
+    <input type="text" value="#FFFFFF" maxlength="7">
     <button onclick="removerCor(this)">X</button>
   `;
   document.getElementById("inputs").appendChild(box);
@@ -20,8 +20,8 @@ function sincronizarCores() {
   document.querySelectorAll(".corBox").forEach(box => {
     let color = box.querySelector("input[type=color]");
     let hex = box.querySelector("input[type=text]");
+    if (!color || !hex || color.id === "corFundo") return;
 
-    // garante que sempre começa com #
     if (!hex.value.startsWith("#")) {
       hex.value = "#" + hex.value.replace("#", "");
     }
@@ -31,33 +31,18 @@ function sincronizarCores() {
     }
 
     hex.oninput = function () {
-
-      let valor = hex.value;
-
-      // remove todos os # e coloca apenas um no começo
-      valor = "#" + valor.replace(/#/g, "");
-
-      // limita para # + 6 caracteres
-      valor = valor.substring(0, 7);
-
-      hex.value = valor.toUpperCase();
-
-      // atualiza o seletor de cor se for HEX válido
+      let valor = "#" + hex.value.replace(/#/g, "");
+      valor = valor.substring(0, 7).toUpperCase();
+      hex.value = valor;
       if (/^#[0-9A-F]{6}$/i.test(valor)) {
         color.value = valor;
       }
     }
 
-    // impede colar errado (ex: ##FF0000)
     hex.onpaste = function (e) {
       e.preventDefault();
-
-      let texto = e.clipboardData.getData("text");
-
-      texto = texto.replace(/#/g, "");
-
+      let texto = e.clipboardData.getData("text").replace(/#/g, "");
       hex.value = "#" + texto.substring(0, 6).toUpperCase();
-
       if (/^#[0-9A-F]{6}$/i.test(hex.value)) {
         color.value = hex.value;
       }
@@ -66,6 +51,43 @@ function sincronizarCores() {
 }
 
 sincronizarCores();
+
+function sincronizarFundo() {
+  let color = document.getElementById("corFundo");
+  let hex = document.getElementById("hexFundo");
+
+  function aplicarFundo(valor) {
+    document.querySelectorAll(".bloco").forEach(b => b.style.background = valor);
+    criarImagem();
+  }
+
+  color.oninput = function () {
+    hex.value = color.value.toUpperCase();
+    aplicarFundo(color.value);
+  }
+
+  hex.oninput = function () {
+    let valor = "#" + hex.value.replace(/#/g, "");
+    valor = valor.substring(0, 7).toUpperCase();
+    hex.value = valor;
+    if (/^#[0-9A-F]{6}$/i.test(valor)) {
+      color.value = valor;
+      aplicarFundo(valor);
+    }
+  }
+
+  hex.onpaste = function (e) {
+    e.preventDefault();
+    let texto = e.clipboardData.getData("text").replace(/#/g, "");
+    hex.value = "#" + texto.substring(0, 6).toUpperCase();
+    if (/^#[0-9A-F]{6}$/i.test(hex.value)) {
+      color.value = hex.value;
+      aplicarFundo(hex.value);
+    }
+  }
+}
+
+sincronizarFundo();
 
 function criarGrade(cor, classe) {
   let div = document.createElement("div");
@@ -85,46 +107,75 @@ function criarGrade(cor, classe) {
   return div;
 }
 
+function renderizarBlocoOffscreen(c1, c2, fundo) {
+  let tam = 50;
+  let cv = document.createElement("canvas");
+  cv.width = 5 * tam;
+  cv.height = 5 * tam;
+  let cx = cv.getContext("2d");
+
+  let r1 = parseInt(c1.substr(1,2),16), g1 = parseInt(c1.substr(3,2),16), b1 = parseInt(c1.substr(5,2),16);
+  let r2 = parseInt(c2.substr(1,2),16), g2 = parseInt(c2.substr(3,2),16), b2 = parseInt(c2.substr(5,2),16);
+  let op = [1, .75, .5, .25, 0];
+
+  cx.fillStyle = fundo;
+  cx.fillRect(0, 0, cv.width, cv.height);
+
+  for (let linha = 0; linha < 5; linha++) {
+    for (let coluna = 0; coluna < 5; coluna++) {
+      cx.globalCompositeOperation = "source-over";
+      cx.fillStyle = `rgba(${r1},${g1},${b1},${op[coluna]})`;
+      cx.fillRect(coluna * tam, linha * tam, tam, tam);
+    }
+  }
+
+  cx.save();
+  cx.translate(cv.width / 2, cv.height / 2);
+  cx.rotate(Math.PI / 2);
+  cx.translate(-cv.width / 2, -cv.height / 2);
+  cx.globalCompositeOperation = "multiply";
+  for (let linha = 0; linha < 5; linha++) {
+    for (let coluna = 0; coluna < 5; coluna++) {
+      cx.fillStyle = `rgba(${r2},${g2},${b2},${op[coluna]})`;
+      cx.fillRect(coluna * tam, linha * tam, tam, tam);
+    }
+  }
+  cx.restore();
+
+  return cx.getImageData(0, 0, cv.width, cv.height);
+}
+
+function pixelDaImagem(imageData, coluna, linha, tam) {
+  let x = coluna * tam + Math.floor(tam / 2);
+  let y = linha * tam + Math.floor(tam / 2);
+  let idx = (y * imageData.width + x) * 4;
+  return [imageData.data[idx], imageData.data[idx+1], imageData.data[idx+2]];
+}
+
 function gerar() {
   let cores = [];
   document.querySelectorAll(".corBox input[type=color]").forEach(c => {
-    cores.push(c.value);
+    if (c.id !== "corFundo") cores.push(c.value);
   });
+
+  let fundo = document.getElementById("corFundo").value;
   let area = document.getElementById("paletas");
   area.innerHTML = "";
   combinacoes = [];
+
   for (let i = 0; i < cores.length; i++) {
     for (let j = i + 1; j < cores.length; j++) {
       let bloco = document.createElement("div");
       bloco.className = "bloco";
+      bloco.style.background = fundo;
       bloco.appendChild(criarGrade(cores[i], "cam1"));
       bloco.appendChild(criarGrade(cores[j], "cam2"));
       area.appendChild(bloco);
       combinacoes.push([cores[i], cores[j]]);
     }
   }
-  criarImagem();
-}
 
-function desenharGrade(ctx, cor, offsetY, rot) {
-  let r = parseInt(cor.substr(1, 2), 16);
-  let g = parseInt(cor.substr(3, 2), 16);
-  let b = parseInt(cor.substr(5, 2), 16);
-  let op = [1, .75, .5, .25, 0];
-  ctx.save();
-  if (rot) {
-    ctx.translate(125, offsetY + 125);
-    ctx.rotate(Math.PI / 2);
-    ctx.translate(-125, -offsetY - 125);
-    ctx.globalCompositeOperation = "multiply";
-  }
-  for (let linha = 0; linha < 5; linha++) {
-    for (let coluna = 0; coluna < 5; coluna++) {
-      ctx.fillStyle = `rgba(${r},${g},${b},${op[coluna]})`;
-      ctx.fillRect(coluna * 50, linha * 50 + offsetY, 50, 50);
-    }
-  }
-  ctx.restore();
+  criarImagem();
 }
 
 function criarImagem() {
@@ -133,59 +184,62 @@ function criarImagem() {
 
   let cores = [];
   document.querySelectorAll(".corBox input[type=color]").forEach(c => {
-    cores.push(c.value);
+    if (c.id !== "corFundo") cores.push(c.value);
   });
 
+  let fundo = document.getElementById("corFundo").value;
   let tam = 50;
-  let passos = 4;
-  let op = [1, .75, .5, .25];
 
   let combos = [];
   for (let i = 0; i < cores.length; i++)
     for (let j = i + 1; j < cores.length; j++)
       combos.push([cores[i], cores[j]]);
 
-  let linhasExtras = cores.length;
+  let duasCores = cores.length === 2;
+  let grade = duasCores ? 5 : 4;
 
-  canvas.width = passos * tam;
-  canvas.height = (combos.length * passos + linhasExtras) * tam;
+  canvas.width = grade * tam;
+  canvas.height = duasCores
+    ? grade * tam
+    : (combos.length * 4 + cores.length) * tam;
 
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = fundo;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   let offsetY = 0;
 
   combos.forEach(([c1, c2]) => {
-    let r1 = parseInt(c1.substr(1, 2), 16), g1 = parseInt(c1.substr(3, 2), 16), b1 = parseInt(c1.substr(5, 2), 16);
-    let r2 = parseInt(c2.substr(1, 2), 16), g2 = parseInt(c2.substr(3, 2), 16), b2 = parseInt(c2.substr(5, 2), 16);
-    for (let linha = 0; linha < 4; linha++) {
-      for (let coluna = 0; coluna < 4; coluna++) {
-        let ox = coluna * tam;
-        let oy = offsetY + linha * tam;
-        ctx.globalCompositeOperation = "source-over";
-        ctx.fillStyle = `rgba(${r1},${g1},${b1},${op[coluna]})`;
-        ctx.fillRect(ox, oy, tam, tam);
-        ctx.globalCompositeOperation = "multiply";
-        ctx.fillStyle = `rgba(${r2},${g2},${b2},${op[linha]})`;
-        ctx.fillRect(ox, oy, tam, tam);
+    let imageData = renderizarBlocoOffscreen(c1, c2, fundo);
+    for (let linha = 0; linha < grade; linha++) {
+      for (let coluna = 0; coluna < grade; coluna++) {
+        let [r, g, b] = pixelDaImagem(imageData, coluna, linha, tam);
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
+        ctx.fillRect(coluna * tam, offsetY + linha * tam, tam, tam);
       }
     }
-    offsetY += 4 * tam;
+    offsetY += grade * tam;
   });
 
-  ctx.globalCompositeOperation = "source-over";
-
-  cores.forEach(cor => {
-    let r = parseInt(cor.substr(1, 2), 16), g = parseInt(cor.substr(3, 2), 16), b = parseInt(cor.substr(5, 2), 16);
-    for (let coluna = 0; coluna < 4; coluna++) {
-      ctx.fillStyle = `rgba(${r},${g},${b},${op[coluna]})`;
-      ctx.fillRect(coluna * tam, offsetY, tam, tam);
-    }
-    offsetY += tam;
-  });
-
-  ctx.fillStyle = "#ffffff";
-  ctx.fillRect(0, offsetY, canvas.width, tam);
+  if (!duasCores) {
+    cores.forEach(cor => {
+      let combo = combos.find(([c1]) => c1 === cor);
+      if (!combo) combo = combos.find(([c1, c2]) => c2 === cor);
+      if (!combo) return;
+      let imageData = renderizarBlocoOffscreen(combo[0], combo[1], fundo);
+      let isC1 = combo[0] === cor;
+      for (let coluna = 0; coluna < 4; coluna++) {
+        let r, g, b;
+        if (isC1) {
+          [r, g, b] = pixelDaImagem(imageData, coluna, 4, tam);
+        } else {
+          [r, g, b] = pixelDaImagem(imageData, 4, coluna, tam);
+        }
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
+        ctx.fillRect(coluna * tam, offsetY, tam, tam);
+      }
+      offsetY += tam;
+    });
+  }
 }
 
 function toggleDropdown() {
@@ -204,117 +258,79 @@ function baixar(modo) {
   document.getElementById("dropdown").style.display = "none";
 
   let cores = [];
-  document.querySelectorAll(".corBox input[type=color]").forEach(c => cores.push(c.value));
+  document.querySelectorAll(".corBox input[type=color]").forEach(c => {
+    if (c.id !== "corFundo") cores.push(c.value);
+  });
 
-  let op = [1, .75, .5, .25];
+  let fundo = document.getElementById("corFundo").value;
 
   let combos = [];
   for (let i = 0; i < cores.length; i++)
     for (let j = i + 1; j < cores.length; j++)
       combos.push([cores[i], cores[j]]);
 
-  let linhasExtras = cores.length ;
-  let totalLinhas = combos.length * 4 + linhasExtras;
+  let duasCores = cores.length === 2;
+  let grade = duasCores ? 5 : 4;
+  let totalLinhas = duasCores ? 5 : combos.length * 4 + cores.length;
 
   let canvas = document.createElement("canvas");
   let ctx = canvas.getContext("2d");
+  let tam = modo === "1px" ? 1 : 50;
+  let tamOffscreen = 50;
 
-  if (modo === "blend") {
-    let tam = 50;
-    canvas.width = 4 * tam;
-    canvas.height = totalLinhas * tam;
+  canvas.width = grade * tam;
+  canvas.height = totalLinhas * tam;
 
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = fundo;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    let offsetY = 0;
+  let offsetY = 0;
 
-    combos.forEach(([c1, c2]) => {
-      let r1 = parseInt(c1.substr(1, 2), 16), g1 = parseInt(c1.substr(3, 2), 16), b1 = parseInt(c1.substr(5, 2), 16);
-      let r2 = parseInt(c2.substr(1, 2), 16), g2 = parseInt(c2.substr(3, 2), 16), b2 = parseInt(c2.substr(5, 2), 16);
-      for (let linha = 0; linha < 4; linha++) {
-        for (let coluna = 0; coluna < 4; coluna++) {
-          let ox = coluna * tam;
-          let oy = offsetY + linha * tam;
-          ctx.globalCompositeOperation = "source-over";
-          ctx.fillStyle = `rgba(${r1},${g1},${b1},${op[coluna]})`;
-          ctx.fillRect(ox, oy, tam, tam);
-          ctx.globalCompositeOperation = "multiply";
-          ctx.fillStyle = `rgba(${r2},${g2},${b2},${op[linha]})`;
-          ctx.fillRect(ox, oy, tam, tam);
-        }
+  combos.forEach(([c1, c2]) => {
+    let imageData = renderizarBlocoOffscreen(c1, c2, fundo);
+    for (let linha = 0; linha < grade; linha++) {
+      for (let coluna = 0; coluna < grade; coluna++) {
+        let [r, g, b] = pixelDaImagem(imageData, coluna, linha, tamOffscreen);
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
+        ctx.fillRect(coluna * tam, offsetY + linha * tam, tam, tam);
       }
-      offsetY += 4 * tam;
-    });
+    }
+    offsetY += grade * tam;
+  });
 
-    ctx.globalCompositeOperation = "source-over";
-
+  if (!duasCores) {
     cores.forEach(cor => {
-      let r = parseInt(cor.substr(1, 2), 16), g = parseInt(cor.substr(3, 2), 16), b = parseInt(cor.substr(5, 2), 16);
+      let combo = combos.find(([c1]) => c1 === cor);
+      if (!combo) combo = combos.find(([c1, c2]) => c2 === cor);
+      if (!combo) return;
+      let imageData = renderizarBlocoOffscreen(combo[0], combo[1], fundo);
+      let isC1 = combo[0] === cor;
       for (let coluna = 0; coluna < 4; coluna++) {
-        ctx.fillStyle = `rgba(${r},${g},${b},${op[coluna]})`;
+        let r, g, b;
+        if (isC1) {
+          [r, g, b] = pixelDaImagem(imageData, coluna, 4, tamOffscreen);
+        } else {
+          [r, g, b] = pixelDaImagem(imageData, 4, coluna, tamOffscreen);
+        }
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
         ctx.fillRect(coluna * tam, offsetY, tam, tam);
       }
       offsetY += tam;
     });
+  }
 
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, offsetY, canvas.width, tam);
-
-    // cria um canvas desfocado pra exportar
+  if (modo === "blend") {
     let canvasBlur = document.createElement("canvas");
     canvasBlur.width = canvas.width;
     canvasBlur.height = canvas.height;
     let ctxBlur = canvasBlur.getContext("2d");
-    ctxBlur.filter = "blur(20px)";
+    ctxBlur.filter = "blur(15px)";
     ctxBlur.drawImage(canvas, 0, 0);
-
     let link = document.createElement("a");
     link.download = "paleta-blend.png";
     link.href = canvasBlur.toDataURL("image/png");
     link.click();
     return;
-  } else {
-    let tam = modo === "1px" ? 1 : 50;
-    canvas.width = 4 * tam;
-    canvas.height = totalLinhas * tam;
-
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    let offsetY = 0;
-
-    combos.forEach(([c1, c2]) => {
-      let r1 = parseInt(c1.substr(1, 2), 16), g1 = parseInt(c1.substr(3, 2), 16), b1 = parseInt(c1.substr(5, 2), 16);
-      let r2 = parseInt(c2.substr(1, 2), 16), g2 = parseInt(c2.substr(3, 2), 16), b2 = parseInt(c2.substr(5, 2), 16);
-      for (let linha = 0; linha < 4; linha++) {
-        for (let coluna = 0; coluna < 4; coluna++) {
-          let ox = coluna * tam;
-          let oy = offsetY + linha * tam;
-          ctx.globalCompositeOperation = "source-over";
-          ctx.fillStyle = `rgba(${r1},${g1},${b1},${op[coluna]})`;
-          ctx.fillRect(ox, oy, tam, tam);
-          ctx.globalCompositeOperation = "multiply";
-          ctx.fillStyle = `rgba(${r2},${g2},${b2},${op[linha]})`;
-          ctx.fillRect(ox, oy, tam, tam);
-        }
-      }
-      offsetY += 4 * tam;
-    });
-
-    ctx.globalCompositeOperation = "source-over";
-
-    cores.forEach(cor => {
-      let r = parseInt(cor.substr(1, 2), 16), g = parseInt(cor.substr(3, 2), 16), b = parseInt(cor.substr(5, 2), 16);
-      for (let coluna = 0; coluna < 4; coluna++) {
-        ctx.fillStyle = `rgba(${r},${g},${b},${op[coluna]})`;
-        ctx.fillRect(coluna * tam, offsetY, tam, tam);
-      }
-      offsetY += tam;
-    });
-
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, offsetY, canvas.width, tam);
   }
 
   let link = document.createElement("a");
